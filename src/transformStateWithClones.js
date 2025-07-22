b295d51397faff93010fe5532dc9d53bf7e967cf
BadOfficer <tarasbondarenko2005@gmail.com>
2025-07-23 01:53:38 +0300

    Solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,33 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateHistory = [];
+  let copyState = { ...state };
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'clear':
+        copyState = {};
+        break;
+
+      case 'addProperties':
+        Object.assign(copyState, action.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const property of action.keysToRemove) {
+          delete copyState[property];
+        }
+        break;
+
+      default:
+        throw new Error('Unexpected action');
+    }
+
+    stateHistory.push({ ...copyState });
+  }
+
+  return stateHistory;
 }
 
 module.exports = transformStateWithClones;
